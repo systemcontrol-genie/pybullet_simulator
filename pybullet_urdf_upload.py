47860495353ee92ac87d31b8f8df9e49557b0b79
systemcontrol-genie <62083334+systemcontrol-genie@users.noreply.github.com>
2024-09-05 12:23:03 +0900

    Update pybullet_urdf_upload.py

--- pybullet_urdf_upload.py
+++ pybullet_urdf_upload.py
@@ -2,15 +2,21 @@ import pybullet as p
 import pybullet_data
 import time
 
-p.connect(p.GUI) # 시뮬레이터 GUI 형성
-p.setGravity(0,0,-9.8) # 중력 적용
+class robot_upload:
+    def __init__(self):
+        # simulator gui 설정, 물리 중력 설정 
+        p.connect(p.GUI)
+        p.setGravity(0,0,-9.8)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        planeId = p.loadURDF('plane.urdf')
+        robot = p.loadURDF('/home/zenbook/pybullet_ros/Lite6/lite6.urdf', basePosition  = [0,0,0])
+    def main(self):
+        while True:   
+            p.stepSimulation()
+            time.sleep(1/240)
+        p.disconnect()
 
-p.setAdditionalSearchPath(pybullet_data.getDataPath())  # URDF 파일 경로 설정
-planeId = p.loadURDF("plane.urdf")
 
-ROBOT = p.loadURDF('/home/zenbook/pybullet_ros/Lite6/lite6.urdf', basePosition = [0,0,0])
-#urdf 파일 업로드
-while True:
-    p.stepSimulation()
-    time.sleep(1/240)
-p.disconnect()
+if __name__ =="__main__":
+    control_arm = robot_upload()
+    control_arm.main()
